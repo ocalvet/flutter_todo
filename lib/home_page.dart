f2528daa08c759f4f8c3e9a3d84d97e7bdfe106e
Ovidio R. Calvet <calveto@gmail.com>
2018-11-23 11:27:49 -0500

    Add listing of todos to page

---
+++ lib/home_page.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_todo/todo.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({Key key, this.title}) : super(key: key);
+  final String title;
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<Todo> _todos = [];
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  addTodo() {
+    this.setState(() {
+      _todos = List.from(_todos)..add(Todo('test', 'testing', false));
+    });
+  }
+
+  completeTodo(todo) {
+    return (bool completed) {
+    };
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: ListView(
+        children: _todos.map((todo) => ListTile(
+          leading: Checkbox(
+            value: todo.completed, 
+            onChanged: completeTodo(todo)),
+          title: Text(todo.title),
+          subtitle: Text(todo.description),
+        )).toList(),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: addTodo,
+        tooltip: 'Add Todo',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
---
+++ lib/todo.dart
@@ -0,0 +1,6 @@
+class Todo {
+  final String title;
+  final String description;
+  final bool completed;
+  Todo(this.title, this.description, this.completed);
+}
