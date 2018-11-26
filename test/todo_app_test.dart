faeec2c998e5ff0c1cff06dcd9b519500e9012c1
Ovidio R. Calvet <calveto@gmail.com>
2018-11-26 16:12:03 -0500

    Add basic unit testing

--- test/todo_app_test.dart
+++ test/todo_app_test.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_todo/todo_app.dart';
+import 'package:flutter_todo/todos_page.dart';
 
 void main() {
-  testWidgets('my first widget test', (WidgetTester tester) async {
-    var app = TodoApp();
+  testWidgets('Finds the MaterialApp widget', (WidgetTester tester) async {
     await tester.pumpWidget(TodoApp());
-
     expect(find.byType(MaterialApp), findsOneWidget);
   });
+  testWidgets('Finds the TodosPage widget', (WidgetTester tester) async {
+    await tester.pumpWidget(TodoApp());
+    expect(find.byType(TodosPage), findsOneWidget);
+  });
+
 }
