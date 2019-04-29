5f116389e18275c9c27cfc55edb30ac417981427
Ovidio R. Calvet <calveto@gmail.com>
2019-04-29 08:12:36 -0400

    Add loading screen

--- lib/todo_app.dart
+++ lib/todo_app.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_todo/screens/add_todo_screen.dart';
 import 'package:flutter_todo/screens/edit_todo_screen.dart';
+import 'package:flutter_todo/screens/loading_screen.dart';
 import 'package:flutter_todo/screens/login_screen.dart';
 import 'package:flutter_todo/screens/todos_screen.dart';
 import 'package:flutter_todo/todos_bloc.dart';
@@ -14,19 +15,21 @@ class TodoApp extends StatelessWidget {
         stream: todosBloc.auth$,
         builder: (context, snapshot) {
           bool authenticated = snapshot.hasData && snapshot.data.token != null;
-          return MaterialApp(
-            debugShowCheckedModeBanner: false,
-            title: 'Todoapp',
-            theme: ThemeData(
-              primarySwatch: Colors.teal,
-            ),
-            routes: <String, WidgetBuilder>{
-              '/': (BuildContext context) =>
-                  authenticated ? TodosScreen() : LoginScreen(),
-              '/add-todo': (BuildContext context) => AddTodoScreen(),
-              '/edit-todo': (BuildContext context) => EditTodoScreen(),
-            },
-          );
+          return !snapshot.hasData
+              ? LoadingScreen()
+              : MaterialApp(
+                  debugShowCheckedModeBanner: false,
+                  title: 'Todoapp',
+                  theme: ThemeData(
+                    primarySwatch: Colors.teal,
+                  ),
+                  routes: <String, WidgetBuilder>{
+                    '/': (BuildContext context) =>
+                        authenticated ? TodosScreen() : LoginScreen(),
+                    '/add-todo': (BuildContext context) => AddTodoScreen(),
+                    '/edit-todo': (BuildContext context) => EditTodoScreen(),
+                  },
+                );
         });
   }
 }
