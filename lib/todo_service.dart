bbc0444ad848cce92fbe5cc571fa8b553bc30f4c
Ovidio R. Calvet <calveto@gmail.com>
2019-05-04 23:10:58 -0400

    Remove try catch when getting todos

--- lib/todo_service.dart
+++ lib/todo_service.dart
@@ -7,13 +7,9 @@ class TodoService {
   final String apiUrl = "http://localhost:6000/api/todos";
 
   Future<TodoList> getTodos() async {
-    try {
-      Response response = await Dio().get(apiUrl);
-      var todos = List<Todo>();
-      response.data.forEach((todoJson) => todos.add(Todo.fromJson(todoJson)));
-      return TodoList(todos: todos);
-    } catch (e) {
-      print(e);
-    }
+    Response response = await Dio().get(apiUrl);
+    var todos = List<Todo>();
+    response.data.forEach((todoJson) => todos.add(Todo.fromJson(todoJson)));
+    return TodoList(todos: todos);
   }
 }
