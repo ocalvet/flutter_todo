52ad03f29216ef903b570ee61cbee3ab76a365c0
Ovidio R. Calvet <calveto@gmail.com>
2019-05-08 22:21:33 -0400

    Add authentication bloc

--- lib/authentication/authentication_bloc.dart
+++ lib/authentication/authentication_bloc.dart
@@ -1,13 +1,33 @@
+import 'dart:async';
+
 import 'package:bloc/bloc.dart';
+import 'package:flutter_todo/authentication/authentication_event.dart';
+import 'package:flutter_todo/authentication/authentication_state.dart';
 
-class AuthenticationBloc extends Bloc {
+class AuthenticationBloc
+    extends Bloc<AuthenticationEvent, AuthenticationState> {
   @override
-  // TODO: implement initialState
-  get initialState => null;
+  get initialState => AuthenticationUnauthenticated();
 
   @override
-  Stream mapEventToState(event) {
-    // TODO: implement mapEventToState
-    return null;
+  Stream<AuthenticationState> mapEventToState(
+      AuthenticationEvent event) async* {
+    if (event is AppStarted) {
+      // TODO add logic here
+      yield AuthenticationAuthenticated();
+    }
+
+    if (event is LoggedIn) {
+      yield AuthenticationLoading();
+      // await userRepository.persistToken(event.token);
+      await Future.delayed(Duration(seconds: 2));
+      yield AuthenticationAuthenticated();
+    }
+
+    if (event is LoggedOut) {
+      yield AuthenticationLoading();
+      // await userRepository.deleteToken();
+      yield AuthenticationUnauthenticated();
+    }
   }
 }
