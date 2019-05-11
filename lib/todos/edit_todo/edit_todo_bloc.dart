939c062ef556b3d2afc7631b73a255fad38a9d97
Ovidio R. Calvet <calveto@gmail.com>
2019-05-11 09:53:39 -0400

    Create edit todo bloc

---
+++ lib/todos/edit_todo/edit_todo_bloc.dart
@@ -0,0 +1,13 @@
+import 'package:bloc/bloc.dart';
+import 'package:flutter_todo/todos/edit_todo/edit_todo_events.dart';
+import 'package:flutter_todo/todos/edit_todo/edit_todo_state.dart';
+
+class EditTodoBloc extends Bloc<EditTodoEvent, EditTodoState> {
+  @override
+  EditTodoState get initialState => InitialEditTodo();
+
+  @override
+  Stream<EditTodoState> mapEventToState(EditTodoEvent event) async* {
+    yield InitialEditTodo();
+  }
+}
