fbba97ca0a5ab2aec767ce3c48f84b3cf1e9905b
Ovidio R. Calvet <calveto@gmail.com>
2019-05-23 07:39:11 -0400

    Update events

--- lib/photo_manager/bloc/photo_manager_event.dart
+++ lib/photo_manager/bloc/photo_manager_event.dart
@@ -1,7 +1,19 @@
 import 'package:equatable/equatable.dart';
+import 'package:flutter/painting.dart';
+import 'package:flutter_todo/models/models.dart';
 import 'package:meta/meta.dart';
 
 @immutable
 abstract class PhotoManagerEvent extends Equatable {
   PhotoManagerEvent([List props = const []]) : super(props);
+}
+
+class LoadPhotos extends PhotoManagerEvent {
+  final Todo todo;
+  LoadPhotos(this.todo) : super([todo]);
+}
+
+class AddPhoto extends PhotoManagerEvent {
+  final ImageInfo photo;
+  AddPhoto(this.photo) : super([photo]);
 }
